9766796bb61decddddb7bb70b0d18c2432d4f2c9
anhthii <nguyendanganhthi247@gmail.com>
2024-08-15 22:34:07 +0700

    Experimental get balances at block number

--- src/application.ts
+++ src/application.ts
@@ -1,9 +1,13 @@
-import ololog from 'ololog';
-import { ethers } from 'ethers';
-import { Config } from './interface';
-import { Evm } from './providers/evm';
-import { EndOfEvents, UniV2HoldingsResponse, TokenBalance } from './providers/interface';
-import { ObjectConfig } from './object-config';
+import ololog from "ololog";
+import { ethers } from "ethers";
+import { Config } from "./interface";
+import { Evm } from "./providers/evm";
+import {
+  EndOfEvents,
+  UniV2HoldingsResponse,
+  TokenBalance,
+} from "./providers/interface";
+import { ObjectConfig } from "./object-config";
 
 export class Application {
   readonly config: Config;
@@ -16,50 +20,77 @@ export class Application {
     this.log = ololog.configure({ locate: false, time: true });
   }
 
-  async indexUniV2PoolPromised(stringifyBalances = false): Promise<UniV2HoldingsResponse> {
-    return new Promise(async resolve => {
-      await this.indexUniV2Pool(holdings => resolve(holdings), stringifyBalances);
+  async indexUniV2PoolPromised(
+    stringifyBalances = false
+  ): Promise<UniV2HoldingsResponse> {
+    return new Promise(async (resolve) => {
+      await this.indexUniV2Pool(
+        (holdings) => resolve(holdings),
+        stringifyBalances
+      );
     });
   }
 
-  async indexUniV2Pool(onEnd?: Function, stringifyBalances = false): Promise<Function> {
-    return this.index(async balances => {
-      const evm = new Evm(this.config.get('rpc') as string);
+  async indexUniV2Pool(
+    onEnd?: Function,
+    stringifyBalances = false
+  ): Promise<Function> {
+    return this.index(
+      async (balances) => {
+        const evm = new Evm(this.config.get("rpc") as string);
 
-      this.logging && this.log.darkGray(
-        `Calculating ${balances.size} LPs holdings in "${this.config.get('token')}" Uni V2 pool`
-      );
-      const holdings = await evm.getUniV2Holdings(
-        this.config.get('token') as string,
-        balances,
-        this.config.get('end') as number,
-        stringifyBalances,
-      );
+        this.logging &&
+          this.log.darkGray(
+            `Calculating ${balances.size} LPs holdings in "${this.config.get(
+              "token"
+            )}" Uni V2 pool`
+          );
+        const holdings = await evm.getUniV2Holdings(
+          this.config.get("token") as string,
+          balances,
+          this.config.get("end") as number,
+          stringifyBalances
+        );
 
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      return (onEnd ?? (() => { }))(holdings);
-    }, true, false); // doesn't make sense if no balances read 
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        return (onEnd ?? (() => {}))(holdings);
+      },
+      true,
+      false
+    ); // doesn't make sense if no balances read
   }
 
-  async indexPromised(readBalances = false, stringifyBalances = false): Promise<Set<TokenBalance>> {
-    return new Promise(async resolve => {
-      await this.index(balances => resolve(balances), readBalances, stringifyBalances);
+  async indexPromised(
+    readBalances = false,
+    stringifyBalances = false
+  ): Promise<Set<TokenBalance>> {
+    return new Promise(async (resolve) => {
+      await this.index(
+        (balances) => resolve(balances),
+        readBalances,
+        stringifyBalances
+      );
     });
   }
 
-  async index(onEnd?: Function, readBalances = false, stringifyBalances = false): Promise<Function> {
-    const evm = new Evm(this.config.get('rpc') as string);
+  async index(
+    onEnd?: Function,
+    readBalances = false,
+    stringifyBalances = false
+  ): Promise<Function> {
+    const evm = new Evm(this.config.get("rpc") as string);
 
     this.logging && this.log.green(await evm.info());
-    this.logging && this.log.green(`TOKEN=${this.config.get('token')}`);
-    this.logging && this.log.green(`DENOMINATION=${this.config.get('denomination')}`);
-    this.logging && this.log.green(`START_BLOCK=${this.config.get('start')}`);
-    this.logging && this.log.green(`END_BLOCK=${this.config.get('end')}`);
+    this.logging && this.log.green(`TOKEN=${this.config.get("token")}`);
+    this.logging &&
+      this.log.green(`DENOMINATION=${this.config.get("denomination")}`);
+    this.logging && this.log.green(`START_BLOCK=${this.config.get("start")}`);
+    this.logging && this.log.green(`END_BLOCK=${this.config.get("end")}`);
 
     const uniqueAddresses = readBalances ? new Set() : null;
     const cancel = await evm.listenErc20Transfers(
-      this.config.get('token') as string,
-      this.config.get('denomination') as number,
+      this.config.get("token") as string,
+      this.config.get("denomination") as number,
       async (event) => {
         if (event == EndOfEvents) {
           let balances = null;
@@ -67,32 +98,42 @@ export class Application {
           if (readBalances) {
             uniqueAddresses.delete(ethers.constants.AddressZero);
 
-            this.logging && this.log.darkGray(`Read balances of ${uniqueAddresses.size} addresses`);
+            this.logging &&
+              this.log.darkGray(
+                `Read balances of ${uniqueAddresses.size} addresses`
+              );
             balances = await evm.getErc20Balances(
-              this.config.get('token') as string,
+              this.config.get("token") as string,
               uniqueAddresses as Set<string>,
-              stringifyBalances
+              stringifyBalances,
+              this.config.get("end") as number
             );
-            this.logging && this.log.darkGray(`Found ${balances.size} addresses w/ positive balances`);
+            this.logging &&
+              this.log.darkGray(
+                `Found ${balances.size} addresses w/ positive balances`
+              );
           }
 
           // eslint-disable-next-line @typescript-eslint/no-empty-function
-          return (onEnd ?? (() => { }))(balances);
+          return (onEnd ?? (() => {}))(balances);
         }
 
-        this.logging && this.log.yellow(`[${event.blockNumber}] ${event.type}(${event.fromShort}, ${event.toShort}, ${event.amountHuman})`);
+        this.logging &&
+          this.log.yellow(
+            `[${event.blockNumber}] ${event.type}(${event.fromShort}, ${event.toShort}, ${event.amountHuman})`
+          );
 
         if (readBalances) {
           uniqueAddresses.add(event.from);
           uniqueAddresses.add(event.to);
         }
       },
-      this.config.get('start') as number,
-      this.config.get('end') as number,
+      this.config.get("start") as number,
+      this.config.get("end") as number
     );
 
     return () => {
-      this.logging && this.log.yellow('Stopping...');
+      this.logging && this.log.yellow("Stopping...");
       cancel();
     };
   }
--- src/providers/evm.ts
+++ src/providers/evm.ts
@@ -1,9 +1,24 @@
-import { ethers } from 'ethers';
-import { Contract as MulticallContract, Provider as MulticallProvider } from '@ixswap1/ethers-multicall';
-import { ProviderInfo, Abi, Event, EventType, LatestBlock, EndOfEvents, TokenBalance, UniV2Holdings, UniV2HoldingsResponse } from './interface';
+import { ethers } from "ethers";
+import {
+  Contract as MulticallContract,
+  Provider as MulticallProvider,
+} from "@ixswap1/ethers-multicall";
+import {
+  ProviderInfo,
+  Abi,
+  Event,
+  EventType,
+  LatestBlock,
+  EndOfEvents,
+  TokenBalance,
+  UniV2Holdings,
+  UniV2HoldingsResponse,
+} from "./interface";
 
 export class Evm {
-  readonly provider: ethers.providers.JsonRpcProvider | ethers.providers.WebSocketProvider;
+  readonly provider:
+    | ethers.providers.JsonRpcProvider
+    | ethers.providers.WebSocketProvider;
 
   constructor(rpcUrl: string) {
     this.provider = /^wss:/i.test(rpcUrl)
@@ -11,12 +26,20 @@ export class Evm {
       : new ethers.providers.JsonRpcProvider(rpcUrl);
   }
 
-  erc20Contract(token: string, multicall = false, includeUniV2 = false): ethers.Contract | MulticallContract {
+  erc20Contract(
+    token: string,
+    multicall = false,
+    includeUniV2 = false
+  ): ethers.Contract | MulticallContract {
     const abi = [
-      Abi.Transfer, Abi.BalanceOf, Abi.TotalSupply,
-      ...(includeUniV2 ? [Abi.Token0, Abi.Token1, Abi.GetReserves] : [])
+      Abi.Transfer,
+      Abi.BalanceOf,
+      Abi.TotalSupply,
+      ...(includeUniV2 ? [Abi.Token0, Abi.Token1, Abi.GetReserves] : []),
     ];
-    return multicall ? new MulticallContract(token, abi) : new ethers.Contract(token, abi, this.provider);
+    return multicall
+      ? new MulticallContract(token, abi)
+      : new ethers.Contract(token, abi, this.provider);
   }
 
   async getUniV2Holdings(
@@ -36,14 +59,17 @@ export class Evm {
     const [reserve0, reserve1] = await contract.getReserves({ blockTag });
 
     const result = {
-      token0, token1,
+      token0,
+      token1,
       totalSupply: stringifyBalances ? totalSupply.toString() : totalSupply,
       balances: new Set() as Set<UniV2Holdings>,
     };
 
     for (const { address, amount } of balances) {
       // @todo should it be enough for smaller holdings?
-      const perc = (amount as ethers.BigNumber).mul(10 ** 8).div(totalSupply.div(100));
+      const perc = (amount as ethers.BigNumber)
+        .mul(10 ** 8)
+        .div(totalSupply.div(100));
       const amount0 = reserve0.mul(perc).div(10 ** 10);
       const amount1 = reserve1.mul(perc).div(10 ** 10);
 
@@ -58,21 +84,31 @@ export class Evm {
     return result;
   }
 
-  async getErc20Balances(token: string, addresses: Set<string>, stringifyBalances = false)
-    : Promise<Set<TokenBalance>> {
+  async getErc20Balances(
+    token: string,
+    addresses: Set<string>,
+    stringifyBalances = false,
+    block: number | string = LatestBlock
+  ): Promise<Set<TokenBalance>> {
     const ethcallProvider = new MulticallProvider(this.provider);
     await ethcallProvider.init(); // Only required when `chainId` is not provided in the `Provider` constructor
 
     const contract = this.erc20Contract(token, true);
     const result = new Set();
-    const balances = await ethcallProvider.all(Array.from(addresses).map(address => contract.balanceOf(address)));
+    const balances = await ethcallProvider.all(
+      Array.from(addresses).map((address) => contract.balanceOf(address))
+      // block
+    );
 
     let i = 0;
     for (const address of addresses) {
       const balance = balances[i++];
 
-      if (balance.toString() !== '0') {
-        result.add({ address, amount: stringifyBalances ? balance.toString() : balance });
+      if (balance.toString() !== "0") {
+        result.add({
+          address,
+          amount: stringifyBalances ? balance.toString() : balance,
+        });
       }
     }
 
@@ -84,17 +120,36 @@ export class Evm {
     denomination: number,
     callback: Function,
     fromBlock: number = null,
-    toBlock: number | string = LatestBlock,
+    toBlock: number | string = LatestBlock
   ): Promise<Function> {
     const contract = this.erc20Contract(token);
-    const allTransfers = contract.filters.Transfer(null, null) as ethers.EventFilter;
-    const listener = async (from: string, to: string, amount: ethers.BigNumber, { blockNumber }) =>
-      await callback(Evm.wrapTransfer(from, to, amount, denomination, blockNumber));
+    const allTransfers = contract.filters.Transfer(
+      null,
+      null
+    ) as ethers.EventFilter;
+    const listener = async (
+      from: string,
+      to: string,
+      amount: ethers.BigNumber,
+      { blockNumber }
+    ) =>
+      await callback(
+        Evm.wrapTransfer(from, to, amount, denomination, blockNumber)
+      );
 
     if (fromBlock !== null) {
-      const events = await contract.queryFilter(allTransfers, Number(fromBlock), Number(toBlock));
+      const events = await contract.queryFilter(
+        allTransfers,
+        Number(fromBlock),
+        Number(toBlock)
+      );
 
-      for (const { args: [from, to, amount], blockNumber } of events) {
+      console.info("events", events);
+
+      for (const {
+        args: [from, to, amount],
+        blockNumber,
+      } of events) {
         await listener(from, to, amount, { blockNumber });
       }
 
@@ -102,7 +157,7 @@ export class Evm {
       if (toBlock !== LatestBlock) {
         await callback(EndOfEvents);
         // eslint-disable-next-line @typescript-eslint/no-empty-function
-        return () => { }; // no nothing
+        return () => {}; // no nothing
       }
     }
 
@@ -114,13 +169,21 @@ export class Evm {
     const { name, chainId, ensAddress } = await this.provider.getNetwork();
 
     return {
-      name, chainId, ensAddress,
+      name,
+      chainId,
+      ensAddress,
       gasPrice: (await this.provider.getGasPrice()).toString(),
       latestBlock: await this.provider.getBlockNumber(),
     };
   }
 
-  static wrapTransfer(from: string, to: string, amount: ethers.BigNumber, denomination: number, blockNumber: number): Event {
+  static wrapTransfer(
+    from: string,
+    to: string,
+    amount: ethers.BigNumber,
+    denomination: number,
+    blockNumber: number
+  ): Event {
     let type = EventType.Transfer;
 
     if (from == ethers.constants.AddressZero) {
@@ -130,7 +193,11 @@ export class Evm {
     }
 
     return {
-      type, from, to, amount, blockNumber,
+      type,
+      from,
+      to,
+      amount,
+      blockNumber,
       amountHuman: ethers.utils.formatUnits(amount, denomination),
       fromShort: this.shortAddress(from),
       toShort: this.shortAddress(to),
@@ -138,6 +205,8 @@ export class Evm {
   }
 
   static shortAddress(address: string): string {
-    return `${address.substring(0, 7)}...${address.substring(address.length - 6)}`;
+    return `${address.substring(0, 7)}...${address.substring(
+      address.length - 6
+    )}`;
   }
 }
